c2afbc1dc2eedc2b1b2d79cb5110999830bbad8c
bebrkrk <128633139+bebrkrk@users.noreply.github.com>
2025-12-31 12:38:19 +0300

    Update script.js
    Fix script.js error

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 const API = "https://script.google.com/macros/s/AKfycbyaHZadQR6eXh25Nzl0au8Cg_Rh5Acg8-dTyNM2isf1c7doNNJ5ShNipyhTAaiNn3EIvg/exec";
+const page = window.location.pathname;
+
+if (page.includes("catalog")) {
+  fetch(`${API}?action=getBooks`)
+    .then(r => r.json())
+    .then(books => {
+      const container = document.getElementById("books");
+      books.forEach(b => {
+        container.innerHTML += `
+          <div class="book" onclick="openBook(${b.id})">
+            <img src="${b.cover_url}">
+            <div>${b.title}</div>
+          </div>
+        `;
+      });
+    });
+}
+
+if (page.includes("book")) {
+  const bookDiv = document.getElementById("book");
+  const id = new URLSearchParams(location.search).get("id");
+
+  fetch(`${API}?action=getBook&book_id=${id}`)
+    .then(r => r.json())
+    .then(data => {
+      bookDiv.innerHTML = `
+        <h2>${data.book.title}</h2>
+        <img src="${data.book.cover_url}">
+      `;
+    });
+}
 
 function login() {
   fetch(`${API}?action=login&nickname=${nick.value}&pin=${pin.value}`)
